5b3c25b4b9fbe52192de603c162f120b7d134edf
Anders Vestergaard <mufas.anders@hotmail.com>
2017-05-11 11:06:13 +0200

    A lot of class bases

---
+++ EtherShip/EtherShip/Component/Animator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace EtherShip
+{
+    class Animator : Component, IUpdateable
+    {
+        public Dictionary<string, Frame> SpriteFrames { get; set; }
+        private SpriteRendere spriteRendere;
+        private int currentIndex;
+        private float fps;
+        public string frameName;
+
+        public Animator(GameObject obj, SpriteRendere spriteRendere, int currentIndex, float fps, string frameName) : base(obj)
+        {
+
+        }
+
+        public void Update(GameTime gameTime)
+        {
+
+        }
+
+    }
+}
