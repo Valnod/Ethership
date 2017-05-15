65a5e782a37cb76dca9b626f1a551ac857ce60d0
theni <theni@LAPTOP-Q9ORS9IL>
2017-05-15 11:37:02 +0200

    2

---
+++ EtherShip/EtherShip/Component/SpriteRenderer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
+
+namespace EtherShip
+{
+    class SpriteRenderer : Component, IDrawable, IUpdateable
+    {
+        public Texture2D sprite { get; set; }
+        public string spriteName { get; set; }
+        public float scaleFactor;
+        public Rectangle spriteRectangle { get; set; }
+        public float layerDepth;
+
+
+
+        public SpriteRenderer(GameObject obj, Texture2D sprite, string spriteName, float scaleFactor, Rectangle spriteRectangle) : base(obj)
+        {
+            this.sprite = sprite;
+            this.spriteName = spriteName;
+            this.scaleFactor = scaleFactor;
+        }
+         
+        public void LoadContent(ContentManager content)
+        {
+            sprite = content.Load<Texture2D>(spriteName);
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            //andet projekt har ikke en update i spriteRenderer? 
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(sprite, obj.position, spriteRectangle, Color.White, 1f, Vector2.Zero, scaleFactor, SpriteEffects.None, layerDepth);
+        }
+    }
+}
