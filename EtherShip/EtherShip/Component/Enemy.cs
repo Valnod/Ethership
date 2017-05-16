97a02bf3b2b6b0c64f6adf3e6d97a88b7fbc222d
theni <theni@LAPTOP-Q9ORS9IL>
2017-05-16 12:07:55 +0200

    enemy ai

--- EtherShip/EtherShip/Component/Enemy.cs
+++ EtherShip/EtherShip/Component/Enemy.cs
@@ -15,6 +15,10 @@ namespace EtherShip
         public Vector2 direction;
         public int value;
 
+        List<GridPoint> Route;
+        List<GridPoint> AlternativeRoute;
+        List<GridPoint> NotUsable;
+
         public Enemy(GameObject obj, int health, float speed, int value, Vector2 direction) : base(obj)
         {
             this.health = health;
@@ -30,6 +34,16 @@ namespace EtherShip
 
         public void Move(GameTime gameTime)
         {
+            // skal ændres, men får fjende til at gå mod spiller  
+            //Vector2 V = GameWorld.Instance.gos.Find(g => g.GetComponent("Player") != null).transform.Position - GameObject.transform.Position;
+            for (int x = 0; x < GameWorld.Instance.Map.MapGrid.GetLength(0); x++)
+            {
+                for (int y = 0; y < GameWorld.Instance.Map.MapGrid.GetLength(1); y++)
+                {
+                   // Math.Abs((GameWorld.Instance.Map.MapGrid[x, y].Pos - obj.position).Length)
+   
+                }
+            }
 
         }
 
