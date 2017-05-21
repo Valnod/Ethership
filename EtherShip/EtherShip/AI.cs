1cb1092d57d7a8bedcef486a95ff284873750f65
theni <theni@LAPTOP-Q9ORS9IL>
2017-05-21 13:05:11 +0200

    tin gog sager til pathfinder

---
+++ EtherShip/EtherShip/AI.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EtherShip
+{
+    class AI
+    {
+        public List<GridPoint> Pathfind(GridPoint start, GridPoint end)
+        {
+            //GridPoints that have been checked and put in closed list
+            var CheckedGridPoints = new List<GridPoint>();
+
+            //GridPoints next to other points who have been checked
+            var UnCheckedGridPoints = new List<GridPoint> { start };  
+        }
+    }
+}
