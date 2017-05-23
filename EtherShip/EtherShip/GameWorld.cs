8bb43b966d148ba47ec1686066dd2ee18fac4d7f
Daniel <lykke.daniel@hotmail.dk>
2017-05-23 10:31:36 +0200

    Okay

--- EtherShip/EtherShip/GameWorld.cs
+++ EtherShip/EtherShip/GameWorld.cs
@@ -108,9 +108,7 @@ namespace EtherShip
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-            {
                 Exit();
-            }
 
             // TODO: Add your update logic here
 
